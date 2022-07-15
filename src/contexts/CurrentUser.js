5c48bfcb2829c1904cc4c941b38c467a8839214e
skim1127 <skim11279@gmail.com>
2022-07-14 21:41:33 -0400

    change package.json

--- src/contexts/CurrentUser.js
+++ src/contexts/CurrentUser.js
@@ -8,7 +8,7 @@ function CurrentUserProvider({ children }){
     const [currentUser, setCurrentUser] = useState(null)
     useEffect(() => {
         const getLoggedInUser = async () => {
-            let response = await fetch('http://invulnerable-chocolatine-75206.herokuapp.com/authentication/profile', {
+            let response = await fetch('https://invulnerable-chocolatine-75206.herokuapp.com/authentication/profile', {
                 credentials: 'include'
             })
             let user = await response.json()
